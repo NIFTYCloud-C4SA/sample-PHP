00d98a73e3539b378790bbffb38902bd8f8deb3d
Okawa Yuki <okawa.yuki@nifty.co.jp>
2012-08-23 16:44:43 +0900

    update index.php

--- index.php
+++ index.php
@@ -18,9 +18,6 @@
 	print<<<EOF
 <p>
 $str<br>
-<a href="https://twitter.com/share" class="twitter-share-button" data-lang="ja" data-url="http://{$_SERVER['SERVER_NAME']}/hello.php" data-text="$str" data-count="none" data-hashtags="niftycloudc4sa" data-related="niftycloud_c4sa:NIFTYCloud C4SA">Tweet</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-
 </p>
 EOF;
 ?>
